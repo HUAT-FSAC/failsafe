82f40d68e972f34d1668e3f25ffe2dcfdcc147a9
NekoRectifier <hyx123250@gmail.com>
2023-07-31 16:59:25 +0800

    feat: basic frame has been built

--- include/main.hpp
+++ include/main.hpp
@@ -6,18 +6,22 @@
 // msg include
 #include "common_msgs/HUAT_ASENSING.h"
 #include "sensor_msgs/PointCloud2.h"
+#include "sensor_msgs/Image.h"
+#include "common_msgs/vehicle_cmd.h"
+// control msg
 
-
-ros::NodeHandle nh;
-
-ros::Subscriber cam_sub;
-ros::Subscriber lidar_sub;
-ros::Subscriber imu_sub;
-ros::Publisher control_pub;
+// const defination
+enum FailureType {
+    FAILURE_NO = -1,
+    FAILURE_CAM = 0,
+    FAILURE_IMU = 1,
+    FAILURE_LIDAR = 2
+};
 
 // function define
-void init_subscriber();
+void init(ros::NodeHandle nh);
 void lidar_callback(const sensor_msgs::PointCloud2ConstPtr &original_cloud_ptr);
 void imu_callback(const common_msgs::HUAT_ASENSING::ConstPtr &msgs);
+void alert(int type);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,65 @@
 #include "main.hpp"
 
-void init_subscriber()
+void init(ros::NodeHandle nh)
 {
-    // cam_sub = nh.subscribe("/pylon_camera_node/image_raw", 1, cam_callback);
-    lidar_sub = nh.subscribe("/velodyne_points", 2, lidar_callback);
-    imu_sub = nh.subscribe("/INS/ASENSING", 1, imu_callback);
+    // ros::Subscriber cam_sub = nh.subscribe("/pylon_camera_node/image_raw", 1, cam_callback);
+    ros::Subscriber lidar_sub = nh.subscribe("/velodyne_points", 2, lidar_callback);
+    ros::Subscriber imu_sub = nh.subscribe("/INS/ASENSING", 1, imu_callback);
+
+    ros::Publisher control_pub = nh.advertise<common_msgs::vehicle_cmd>("failsafe_control", 100);
 }
 
-// sensor_msgs/Image
+void alert(int type)
+{
+    if (type == FAILURE_CAM)
+    {
+    }
+    else if (type == FAILURE_IMU)
+    {
+    }
+    else if (type == FAILURE_LIDAR)
+    {
+    }
+    else
+    {
+        ROS_INFO("No failure but alert was activated.");
+    }
+    // control_pub.publish();
+}
+
 void lidar_callback(const sensor_msgs::PointCloud2ConstPtr &original_cloud_ptr)
 {
-
 }
 
 void imu_callback(const common_msgs::HUAT_ASENSING::ConstPtr &msgs)
 {
     // if the msg is corrupted
-
 }
 
-// void cam_callback()
-// {
+// sensor_msgs/Image
+void cam_callback(const sensor_msgs::Image::ConstPtr &msg)
+{
+    int _width = msg->width;
+    int _height = msg->height;
 
-// }
+    // size check
+    if (_width < 1080 && _height < 1920)
+    {
+        alert(0);
+    }
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "failsafe");
 
+    ros::NodeHandle nh;
+
+    ros::Subscriber cam_sub;
+    ros::Subscriber lidar_sub;
+    ros::Subscriber imu_sub;
+    ros::Publisher control_pub;
+
+    init(nh);
     ros::spin();
 }
