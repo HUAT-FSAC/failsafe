ecf68f7e81cfdea0b3b5c2ed5ba90d00799540b6
NekoRectifier <hyx123250@gmail.com>
2023-08-08 18:12:04 +0800

    update

--- include/main.hpp
+++ include/main.hpp
@@ -5,13 +5,14 @@
 
 // msg include
 #include "common_msgs/HUAT_ASENSING.h"
-#include "sensor_msgs/PointCloud2.h"
-#include "sensor_msgs/Image.h"
 #include "common_msgs/vehicle_cmd.h"
+#include "sensor_msgs/Image.h"
+#include "sensor_msgs/PointCloud2.h"
 // control msg
 
 // const defination
-enum FailureType {
+enum FailureType
+{
     FAILURE_NO = -1,
     FAILURE_CAM = 0,
     FAILURE_IMU = 1,
@@ -20,6 +21,7 @@ enum FailureType {
 
 // function define
 void init(ros::NodeHandle nh);
+void cam_callback(const sensor_msgs::Image::ConstPtr &msg);
 void lidar_callback(const sensor_msgs::PointCloud2ConstPtr &original_cloud_ptr);
 void imu_callback(const common_msgs::HUAT_ASENSING::ConstPtr &msgs);
 void alert(int type);
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,31 @@
 
 void init(ros::NodeHandle nh)
 {
-    // ros::Subscriber cam_sub = nh.subscribe("/pylon_camera_node/image_raw", 1, cam_callback);
-    ros::Subscriber lidar_sub = nh.subscribe("/velodyne_points", 2, lidar_callback);
+    ros::Subscriber cam_sub =
+        nh.subscribe("/pylon_camera_node/image_raw", 1, cam_callback);
+    ros::Subscriber lidar_sub =
+        nh.subscribe("/velodyne_points", 2, lidar_callback);
     ros::Subscriber imu_sub = nh.subscribe("/INS/ASENSING", 1, imu_callback);
 
-    ros::Publisher control_pub = nh.advertise<common_msgs::vehicle_cmd>("failsafe_control", 100);
+    ros::Publisher control_pub =
+        nh.advertise<common_msgs::vehicle_cmd>("failsafe_control", 100);
 }
 
 void alert(int type)
 {
     if (type == FAILURE_CAM)
     {
+        ROS_INFO("Camera Failure");
+        // TODO disable camera related modules
+        // TODO send stop cmd
     }
     else if (type == FAILURE_IMU)
     {
+        ROS_INFO("IMU Failure");
     }
     else if (type == FAILURE_LIDAR)
     {
+        ROS_INFO("Lidar Failure");
     }
     else
     {
@@ -54,7 +62,6 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "failsafe");
 
     ros::NodeHandle nh;
-
     ros::Subscriber cam_sub;
     ros::Subscriber lidar_sub;
     ros::Subscriber imu_sub;
